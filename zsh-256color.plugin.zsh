c701ad8154b4214a29a22820412d41fafada66f1
Christian Ludwig <chrissicool@gmail.com>
2015-10-28 19:54:57 +0100

    Obey $TERMINFO
    If $TERMINFO is set, include in search path.
    
    While here, make the script more robust and bash-friendly.

--- zsh-256color.plugin.zsh
+++ zsh-256color.plugin.zsh
@@ -4,13 +4,15 @@
 
 _zsh_terminal_set_256color()
 {
-	[[ $TERM =~ "-256color$" ]] && return
+	[[ "$TERM" =~ "-256color$" ]] && return
+
+	local TERM256="${TERM}-256color"
 
 	# search through ncurses terminfo descriptions
-	for terminfos in "${HOME}/.terminfo" "/etc/terminfo" "/lib/terminfo" "/usr/share/terminfo" ; do
-		if [[ -e "$terminfos"/$TERM[1]/${TERM}-256color || \
-				-e "$terminfos"/${TERM}-256color ]] ; then
-			export TERM="${TERM}-256color"
+	for terminfos in $TERMINFO "$HOME/.terminfo" "/etc/terminfo" "/lib/terminfo" "/usr/share/terminfo" ; do
+		if [[ -e "$terminfos"/$TERM[1]/"$TERM256" || \
+				-e "$terminfos"/"$TERM256" ]] ; then
+			export TERM="$TERM256"
 			return
 		fi
 	done
